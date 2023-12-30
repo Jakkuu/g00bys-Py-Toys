3e086c12f9fe0bb8a334ea501a6f1be2b8bd9357
Jackson Kern <150203443+Jakkuu@users.noreply.github.com>
2023-12-29 22:10:26 -0700

    Adjust Canvas To Connect Tools

--- pages/02_✏️_Draw_Me_A_Picture!.py
+++ pages/02_✏️_Draw_Me_A_Picture!.py
@@ -14,8 +14,14 @@ st.title("Draw Me A Picture!")
 
 canvas_result = st_canvas(background_color="rgba(129,168,141,1.000)",
                           fill_color="rgba(129,168,141,1.000)",
+                          update_streamlit=realtime_update,
+                          stroke_color=stroke_color,
+                          background_color=bg_color,
+                          drawing_mode=drawing_mode,
+                          point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+                          key="canvas",
                           height=600,
-                          width=1418)
+                          width=1418,)
 
 drawing_mode = st.selectbox("Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform"))
 

5fe6b9659381ea222bbf4baa77328fd60bb6fb28
Jackson Kern <150203443+Jakkuu@users.noreply.github.com>
2023-12-29 22:01:59 -0700

    Add Canvas Tools

--- pages/02_✏️_Draw_Me_A_Picture!.py
+++ pages/02_✏️_Draw_Me_A_Picture!.py
@@ -49,6 +49,17 @@ if st.button("Download Image"):
     # Provide a download link
     st.markdown(get_binary_file_downloader_html(temp_file_path, 'Draw_g00by_A_Picture'), unsafe_allow_html=True)
 
+    drawing_mode = st.selectbox("Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform"))
+    
+    stroke_width = st.slider("Stroke width: ", 1, 25, 3)
+    if drawing_mode == 'point':
+        point_display_radius = st.slider("Point display radius: ", 1, 25, 3)
+    stroke_color = st.color_picker("Stroke color hex: ")
+    bg_color = st.color_picker("Background color hex: ", "#eee")
+    bg_image = st.file_uploader("Background image:", type=["png", "jpg"])
+    
+    realtime_update = st.checkbox("Update in realtime", True)
+
 st.markdown("""
     <style>
     [data-testid=stButton] > button {
